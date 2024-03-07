0c5659eed9335d8260ca2d2eff462a6cd5d04d87
Johan Aurel Botouli <aurel.botouli@gmail.com>
2024-03-07 12:50:20 +0000

    finished ami, launch templates and autoscalers

---
+++ modules/ami/variables.tf
@@ -0,0 +1,16 @@
+variable "lighting_id" {
+  type = string
+}
+
+variable "heating_id" {
+  type = string
+}
+
+variable "status_id" {
+  type = string
+}
+
+variable "auth_id" {
+  type = string
+}
+
